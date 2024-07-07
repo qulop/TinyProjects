ed59042a8d1eee6f113b428ed4b1863c5d0db2ee
qulop <qlopsimail@gmail.com>
2024-07-07 11:36:18 +0300

    (Concurrency) atomic_i64.hpp renamed to AtomicI64.hpp

---
+++ concurrency/atomic/AtomicI64.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+extern "C" {
+    #include "impl/atomic_i64.h"
+}
+
+
+// Implementation for int64_t
+class AtomicI64
+{
+    using ValueType = int64_t;
+public:
+    Atomic(ValueType val = 0) :
+        m_storage(val)
+    {}
+
+
+    ValueType Load() const
+    { return __atomicLoad(m_storage);
+        
+     }
+
+    ValueType Exchange(ValueType val)
+    { return __atomicExhange(m_storage, val); }
+
+    void Store(ValueType val)
+    { __atomicStore(m_storage, val); }
+    
+private:
+    AtomicStorage m_storage = nullptr;
+};
